b888026c3f68fc6d01578a2ee0bec6a80db4fc90
Sachin Chauhan <sc3759@gmail.com>
2019-11-28 23:33:22 +0530

    Post will be saved into database

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    blog_url = models.CharField(max_length=300, unique=True)
+    title = models.CharField(max_length=300)
+    featured_image = models.CharField(max_length=300)
+    content = models.TextField()
+    author_name = models.CharField(max_length=300)
+    author_image = models.CharField(max_length=300)
+    author_bio = models.TextField()
+    published_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
--- api/views.py
+++ api/views.py
@@ -1,3 +1,65 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-# Create your views here.
+import requests
+from django.views import View
+from django.http import HttpResponse
+from django.http import JsonResponse
+from bs4 import BeautifulSoup
+from .models import Post
+
+class Home(View):
+    def get(self, request):
+
+        blog_url = request.GET.get('url')
+
+        try:
+            post = Post.objects.get(blog_url=blog_url)
+        except Post.DoesNotExist:
+            post_data = self.fetch_data_from_url(blog_url)
+            post_data['blog_url'] = blog_url
+            post = Post.objects.create(**post_data)
+
+        data = {};
+
+        data['id'] = post.id
+        data['blog_url'] = post.blog_url
+        data['title'] = post.title
+        data['featured_image'] = post.featured_image
+        data['content'] = post.content
+        data['author_name'] = post.author_name
+        data['author_image'] = post.author_image
+        data['author_bio'] = post.author_bio
+        data['published_date'] = post.published_date
+
+        return JsonResponse(data)
+
+    def post(self, request):
+        return HttpResponse("post method")
+
+    def fetch_data_from_url(self, url):
+        page = requests.get(url)
+        html_content = BeautifulSoup(page.content, "html.parser")
+
+        data = {}
+
+        data['title'] = html_content.\
+            find_all('h1', class_="entry-header-title")[0].\
+            get_text().strip()
+        data['featured_image'] = html_content.\
+            find_all('img', class_="attachment-bayleaf-large")[0].\
+            attrs['data-lazy-src']
+        data['content'] = html_content.\
+            find_all('div', class_="entry-content")[0].\
+            get_text()
+        data['published_date'] = html_content.\
+            find_all('time', class_="entry-date")[0].\
+            attrs['datetime']
+        data['author_name'] = html_content.\
+            find_all('span', class_="meta-author")[0].\
+            get_text().strip()
+        data['author_image'] = html_content.\
+            find_all('img', class_="avatar")[0].attrs['data-lazy-src']
+        data['author_bio'] = html_content.\
+            find_all('div', class_="entry-author-bio")[0].get_text()
+
+        return data
